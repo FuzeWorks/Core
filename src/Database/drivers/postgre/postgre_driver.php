b205111e6145cec99f812033a8b2c8c392402286
Abel <abel@techfuze.net>
2016-07-23 17:06:19 +0200

    Changed license headers and version information.
    Version 1.0.0 is now described.

--- src/Database/drivers/postgre/postgre_driver.php
+++ src/Database/drivers/postgre/postgre_driver.php
@@ -24,10 +24,10 @@
  * @copyright Copyright (c) 1996 - 2015, Free Software Foundation, Inc. (http://www.fsf.org/)
  * @license   http://opensource.org/licenses/GPL-3.0 GPLv3 License
  *
- * @link  http://fuzeworks.techfuze.net
+ * @link  http://techfuze.net/fuzeworks
  * @since Version 0.0.1
  *
- * @version Version 0.0.1
+ * @version Version 1.0.0
  */
 
 use FuzeWorks\Core;
